e43758e37b8a0353224e22e9e32b02ebe398ff59
Armalon <armalong@gmail.com>
2020-07-30 20:57:24 +0200

    MobX usage

--- src/App.tsx
+++ src/App.tsx
@@ -8,48 +8,60 @@ import Chat, { ChatMessage } from "./components/Chat/Chat";
 import { Space } from 'antd'
 import { Layout } from 'antd';
 
+import { observer } from "mobx-react"
+import { observable, computed } from 'mobx'
+
 const { Header, Footer, Content } = Layout;
 
 // todo: move to the config
 const chatWindowsCount = 4
 
+@observer
 class App extends React.Component {
     // todo: put in a MobX state
     // and make it reactive
     state = {
         //  Array <ChatMessage> =
-        chatMessages: [
-            {
-                userName: 'user #1',
-                text: 'Some message'
-            },
-            {
-                userName: 'user #3',
-                text: 'Another message'
-            },
-            {
-                userName: 'user #4',
-                text: 'Last message'
-            },
-        ]
     }
 
+    @observable
+    chatMessages = [
+        {
+            userName: 'user #1',
+            text: 'Some message'
+        },
+        {
+            userName: 'user #3',
+            text: 'Another message'
+        },
+        {
+            userName: 'user #4',
+            text: 'Last message'
+        },
+    ]
+
     newMessageAddedHandler = (value: string, chatNumber: number) => {
-        const chatMessages = [...this.state.chatMessages]
-        chatMessages.push({
+        this.chatMessages = [...this.chatMessages]
+        this.chatMessages.push({
             userName: `user #${chatNumber}`,
             text: value
         })
-        this.setState({
-            chatMessages
-        })
+
+        // const chatMessages = [...this.state.chatMessages]
+        // chatMessages.push({
+        //     userName: `user #${chatNumber}`,
+        //     text: value
+        // })
+        // this.setState({
+        //     chatMessages
+        // })
     }
 
     render() {
         const Chats = Array.apply(null, Array(chatWindowsCount)).map((v, k) => {
             return <Chat
                 entered={this.newMessageAddedHandler}
-                chatMessages={this.state.chatMessages}
+                chatMessages={this.chatMessages}
                 serialNumber={k + 1}
                 key={k}
             />
@@ -57,8 +69,7 @@ class App extends React.Component {
 
         return (
             <Layout>
-                <Header>
-                    Welcome to chats!
+                <Header title='Welcome to chats!'>
                 </Header>
                 <Content>
                     <Space direction="vertical" style={{width: '100%'}}>
