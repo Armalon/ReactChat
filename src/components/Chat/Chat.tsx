ed152ee994dced1abe781fa8b7b8c759dcc99c55
Armalon <armalong@gmail.com>
2020-07-30 21:39:12 +0200

    Unused statement

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Card, List, Input, Form } from 'antd'
-import {SearchProps} from "antd/es/input/Search";
+// import {SearchProps} from "antd/es/input/Search";
 
 const { Search } = Input;
 
