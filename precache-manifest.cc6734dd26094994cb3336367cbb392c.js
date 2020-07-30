3b9506be618cc0e7ede6281c5920adc9c6e72697
Armalon <armalong@gmail.com>
2020-07-30 21:47:50 +0200

    Updates

---
+++ precache-manifest.cc6734dd26094994cb3336367cbb392c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4d99d5e0aaf700920f7194d00c2d43",
+    "url": "/ReactChat/index.html"
+  },
+  {
+    "revision": "8ea6209599c28c3999e9",
+    "url": "/ReactChat/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "42dc6f01558328cfc48e",
+    "url": "/ReactChat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ea6209599c28c3999e9",
+    "url": "/ReactChat/static/js/2.8bb10c1e.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/ReactChat/static/js/2.8bb10c1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42dc6f01558328cfc48e",
+    "url": "/ReactChat/static/js/main.efce9134.chunk.js"
+  },
+  {
+    "revision": "ca40cf41c1fc42f8f762",
+    "url": "/ReactChat/static/js/runtime-main.7f2dd68a.js"
+  }
+]);
